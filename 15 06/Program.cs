d28a352e44a7ca030075dd1cd3a7a810704aa368
Luka Semenikhin <117368298+lukasemenikhin@users.noreply.github.com>
2023-06-07 17:18:32 +0400

    Add files via upload

---
+++ 15 06/Program.cs
@@ -0,0 +1,80 @@
+﻿
+//pirveli
+
+//using System;
+
+//public class Program
+//{
+//    public static int CountLetters(string phrase)
+//    {
+//        int letterCount = 0;
+
+//        foreach (char c in phrase)
+//        {
+//            if (char.IsLetter(c))
+//            {
+//                letterCount++;
+//            }
+//        }
+
+//        return letterCount;
+//    }
+
+//    public static void Main(string[] args)
+//    {
+//        Console.WriteLine("sheikvane winadadeba:");
+//        string userPhrase = Console.ReadLine();
+
+//        int letterCount = CountLetters(userPhrase);
+
+//        Console.WriteLine("Number of letters: " + letterCount);
+//    }
+//}
+
+
+//mesame
+
+//public class Program
+//{
+//    public static void PrintPhraseMultipleTimes(string phrase, int numberOfTimes)
+//    {
+//        for (int i = 0; i < numberOfTimes; i++)
+//        {
+//            Console.WriteLine(phrase);
+//        }
+//    }
+
+//    public static void Main(string[] args)
+//    {
+//        Console.WriteLine("Enter a phrase:");
+//        string userPhrase = Console.ReadLine();
+
+//        Console.WriteLine("Enter the number of times to print:");
+//        int numberOfTimes = Convert.ToInt32(Console.ReadLine());
+
+//        PrintPhraseMultipleTimes(userPhrase, numberOfTimes);
+//    }
+//}
+
+
+//mexutee
+
+//public bool IsEven(int number)
+//{
+//    if (number % 2 == 0)
+//    {
+//        return true;
+//    }
+//    else
+//    {
+//        return false;
+//    }
+//}
+
+
+//public void PrintMax(int num1, int num2)
+//{
+//    int max = num1 > num2 ? num1 : num2;
+//    Console.WriteLine("The maximum number is: " + max);
+//}
+
